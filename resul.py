508b29d6768ab6d3f0f4363411dffe02b9dbeae2
resul123f <104219417+resul123f@users.noreply.github.com>
2022-08-26 21:27:46 +0400

    resul.py

---
+++ resul.py
@@ -0,0 +1,10 @@
+def faktoriyel(a):
+    """
+    faktoriali bulmak icin bu fonksiyonu kulanin
+
+    """
+    topla = 1
+    for i in range(0,a):
+        i +=1
+        topla *= i
+    return topla
